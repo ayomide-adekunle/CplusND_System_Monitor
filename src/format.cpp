774e2fc32cab6199caa29952f4ba41627705cbc0
root <mailayomide@gmail.com>
2021-07-17 15:29:36 +0000

    fixed format issues for hr and min

--- src/format.cpp
+++ src/format.cpp
@@ -15,13 +15,25 @@ string Format::ElapsedTime(long sec) {
   min = sec / 60;
   sec = sec % 60;
   
+  std::string hr_str, min_str, sec_str = "";
+  
+  if (hrs < 10){
+   hr_str = "0";
+  }
+  if (min < 10){
+   min_str = "0";
+  }
   if (sec < 10){
-   return std::to_string(hrs) + ":" + std::to_string(min) + ":" +"0"+
-         std::to_string(sec);
+   sec_str = "0";
   }
   
+//   if (sec < 10){
+//    return std::to_string(hrs) + ":" + std::to_string(min) + ":" +"0"+
+//          std::to_string(sec);
+//   }
   
+
   
-  return std::to_string(hrs) + ":" + std::to_string(min) + ":" +
-         std::to_string(sec);
+  return hr_str+std::to_string(hrs) + ":" + min_str+std::to_string(min) + ":" +
+         sec_str+std::to_string(sec);
 }
